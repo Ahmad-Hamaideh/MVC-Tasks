b76aad2aa05f414bd0fa890a563d8ea22566f0c5
Ahmad-Hamaideh <iahmed.hamaideh@gmail.com>
2023-02-01 16:18:26 +0300

    task

---
+++ 2_1/task_1/task_1/Controllers/studentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using task_1.Models;
+
+namespace task_1.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        public ActionResult Index()
+        {
+            student st = new student();
+            st.Name = "Ahmad Hamaideh";
+            st.Age= 21;
+            st.Faculity = "IT";
+
+            return View(st);
+        }
+    }
+}
---
+++ 2_1/task_1/task_1/Models/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace task_1.Models
+{
+    public class student
+    {
+        public int ID { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Faculity { get; set; }
+
+    }
+}
