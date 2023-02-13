9c29d8d3cbfe22e4dc11b1b094e7038b2b407b42
Ahmad-Hamaideh <iahmed.hamaideh@gmail.com>
2023-02-13 23:29:56 +0300

    Api

---
+++ Api/ApiTask/ApiTask/Controllers/CLLApisController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiTask.Models;
+
+namespace ApiTask.Controllers
+{
+    public class CLLApisController : ApiController
+    {
+        private MvcEntities db = new MvcEntities();
+
+        // GET: api/CLLApis
+        public IQueryable<CLLApi> GetCLLApis()
+        {
+            return db.CLLApis;
+        }
+
+        // GET: api/CLLApis/5
+        [ResponseType(typeof(CLLApi))]
+        public IHttpActionResult GetCLLApi(int id)
+        {
+            CLLApi cLLApi = db.CLLApis.Find(id);
+            if (cLLApi == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cLLApi);
+        }
+
+        // PUT: api/CLLApis/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCLLApi(int id, CLLApi cLLApi)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cLLApi.IdCLL)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cLLApi).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CLLApiExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CLLApis
+        [ResponseType(typeof(CLLApi))]
+        public IHttpActionResult PostCLLApi(CLLApi cLLApi)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CLLApis.Add(cLLApi);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (CLLApiExists(cLLApi.IdCLL))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = cLLApi.IdCLL }, cLLApi);
+        }
+
+        // DELETE: api/CLLApis/5
+        [ResponseType(typeof(CLLApi))]
+        public IHttpActionResult DeleteCLLApi(int id)
+        {
+            CLLApi cLLApi = db.CLLApis.Find(id);
+            if (cLLApi == null)
+            {
+                return NotFound();
+            }
+
+            db.CLLApis.Remove(cLLApi);
+            db.SaveChanges();
+
+            return Ok(cLLApi);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CLLApiExists(int id)
+        {
+            return db.CLLApis.Count(e => e.IdCLL == id) > 0;
+        }
+    }
+}
---
+++ Api/ApiTask/ApiTask/Controllers/MGApisController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ApiTask.Models;
+
+namespace ApiTask.Controllers
+{
+    public class MGApisController : Controller
+    {
+        private MvcEntities db = new MvcEntities();
+
+        // GET: MGApis
+        public ActionResult Index(int?id)
+        {
+            var mGApis = db.MGApis.Where(m => m.CLLApi.IdCLL == id );
+            return View(mGApis.ToList());
+        }
+
+        // GET: MGApis/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MGApi mGApi = db.MGApis.Find(id);
+            if (mGApi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mGApi);
+        }
+
+        // GET: MGApis/Create
+        public ActionResult Create()
+        {
+            ViewBag.idCLL = new SelectList(db.CLLApis, "IdCLL", "NameCLL");
+            return View();
+        }
+
+        // POST: MGApis/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idMG,NameMG,idCLL")] MGApi mGApi)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MGApis.Add(mGApi);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCLL = new SelectList(db.CLLApis, "IdCLL", "NameCLL", mGApi.idCLL);
+            return View(mGApi);
+        }
+
+        // GET: MGApis/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MGApi mGApi = db.MGApis.Find(id);
+            if (mGApi == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCLL = new SelectList(db.CLLApis, "IdCLL", "NameCLL", mGApi.idCLL);
+            return View(mGApi);
+        }
+
+        // POST: MGApis/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idMG,NameMG,idCLL")] MGApi mGApi)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mGApi).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCLL = new SelectList(db.CLLApis, "IdCLL", "NameCLL", mGApi.idCLL);
+            return View(mGApi);
+        }
+
+        // GET: MGApis/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MGApi mGApi = db.MGApis.Find(id);
+            if (mGApi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mGApi);
+        }
+
+        // POST: MGApis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MGApi mGApi = db.MGApis.Find(id);
+            db.MGApis.Remove(mGApi);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
